6d7914ca143315d1ae45a02c7cddc1e435d30396
vorasam <162532470+vorasam@users.noreply.github.com>
2026-01-21 23:08:12 +0530

    Update CloudinaryConfig.java

--- src/main/java/com/dev/auth/config/CloudinaryConfig.java
+++ src/main/java/com/dev/auth/config/CloudinaryConfig.java
@@ -10,12 +10,18 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
-    @Bean
-    public Cloudinary cloudinary() {
-        Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dvcw0oqca");
-        config.put("api_key", "112263926558927");
-        config.put("api_secret", "qZVN043Dt0FBb8RIDFtKF8o-pao");
-        return new Cloudinary(config);
-    }
+   @Bean
+public Cloudinary cloudinary(
+        @Value("${cloudinary.cloud-name}") String cloudName,
+        @Value("${cloudinary.api-key}") String apiKey,
+        @Value("${cloudinary.api-secret}") String apiSecret
+) {
+    Map<String, String> config = new HashMap<>();
+    config.put("cloud_name", cloudName);
+    config.put("api_key", apiKey);
+    config.put("api_secret", apiSecret);
+    return new Cloudinary(config);
 }
+
+}
+
