1f98ec16b2cd5ac68c8531b73a76aaca34ad70b7
vorasam <162532470+vorasam@users.noreply.github.com>
2026-01-21 17:56:48 +0530

    Update CorsConfig.java

--- src/main/java/com/dev/auth/config/CorsConfig.java
+++ src/main/java/com/dev/auth/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:4300"));
+        config.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:4300","https://fetflix-admin.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
